c1310198bccc8e2fa93203ac1828055df348f51a
Arty <artiom.developer@gmail.com>
2016-07-23 16:58:09 +0300

    Added the FacebookSDK and the login button

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/',function(req,res){
-	res.sendFile(__dirname + '/html/index.html');
+	res.sendFile(__dirname + '/public/html/index.html');
 });
 
 http.listen(process.env.PORT || 5000,function(){
